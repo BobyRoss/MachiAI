7fe5e91b9920bcbdf9ad36b6bdd549f8e4952cab
BobyRoss <sogo.rush@gmail.com>
2025-08-19 17:28:37 -0700

    created a basic repeating chatbot

--- AIVer1.0.py
+++ AIVer1.0.py
@@ -1,1 +1,14 @@
 memory = {}
+
+while True:
+    user_input = input("You: ")
+
+    if user_input.lower() == "bye":
+        print("AI: Bye!")
+        break
+    if user_input in memory:
+        print("AI:", memory[user_input])
+    else:
+        reply = input("I dont know what to say. how should I respond?")
+        memory[user_input] = reply
+        print("AI: Got it! I will remember that.")
